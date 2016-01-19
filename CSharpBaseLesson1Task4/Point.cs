0d537fb7fd5f3e5b7270b870ad741266f1550e6e
Dmytro Kovalchuk <kovalchuk.dima@gmail.com>
2016-01-19 21:02:53 +0200

    CSharp base, lesson 1, task4

---
+++ CSharpBaseLesson1Task4/Point.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBaseLesson1Task4
+{
+    class Point
+    {
+        public Point(string name, int x, int y)
+        {
+            this.name = name;
+            this.x = x;
+            this.y = y;
+        }
+        private string name = null;
+        private int x;
+        private int y;
+
+        public string Name { get { return name; } }
+        public int X { get { return x; } }
+        public int Y { get { return y; } }
+    }
+}
---
+++ CSharpBaseLesson1Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBaseLesson1Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure frigure = new Figure(new Point("A",1,1), new Point("B", 1,4), new Point("C", 4,4));
+            Console.WriteLine("Type figure {0}, P = ", frigure.Type);
+            frigure.PerimeterCalculator();
+        }
+    }
+}
